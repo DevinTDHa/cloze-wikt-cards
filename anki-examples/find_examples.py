b05f05b90a64eef52b7faf33e2ff2ed69d779e5d
Devin Ha <t.ha@tu-berlin.de>
2024-05-14 20:25:09 +0200

    refactor find_examples

--- anki-examples/find_examples.py
+++ anki-examples/find_examples.py
@@ -6,103 +6,102 @@ import multiprocessing as mp
 from multiprocessing import Manager
 
 
-output_folder = "/media/ducha/SSDSHARED/VN/subs_dump/viet_subs_processed2"
+class CorpusExamples:
 
+    def __init__(
+        self,
+        corpus_folder,
+        sentence_min_words=5,
+        sentence_word_limit=30,
+        num_processes=6,
+    ):
+        self.corpus = self.prepare_corpus(corpus_folder)
+        print("Total Examples", sum([len(c[1]) for c in self.corpus]))
+        self.sentence_min_words = sentence_min_words
+        self.sentence_word_limit = sentence_word_limit
+        self.num_processes = num_processes
+        self.split_size = len(self.corpus) // num_processes
 
-def prepare_corpus(output_folder):
-    """Prepare the corpus from the output folder. The corpus is a list of tuples with (file_name, file_content).
-    """
-    print("Preparing corpus from", output_folder)
-    def get_file(file_name):
-        with open(os.path.join(output_folder, file_name), "r") as f:
-            # Read each line and strip the newline character
-            return set([line.strip() for line in f.readlines()])
+        self.manager = Manager()
+        self.result_queue = self.manager.Queue()
 
-    # List all files in the folder, that are actually vietnamese
-    available_files = [
-        fname for fname in os.listdir(output_folder) if fname.rsplit(".", 4)[1] == "vie"
-    ]
+    def prepare_corpus(self, corpus_folder):
+        """Prepare the corpus from the given folder.
 
-    corpus = [
-        (file_name.rsplit(".", 4)[0], get_file(file_name))
-        for file_name in available_files
-    ]
+        Each file in the folder is a document in the corpus, with an example sentence for each line.
+        The corpus is a list of tuples with (file_name, file_content)."""
 
-    print("Corpus prepared with", len(corpus), "files")
-    return np.array(corpus)
+        print("Preparing corpus from", corpus_folder)
 
+        def get_file(file_name):
+            with open(os.path.join(corpus_folder, file_name), "r") as f:
+                # Read each line and strip the newline character
+                return set([line.strip() for line in f.readlines()])
 
-sentence_min_words = 5
-sentence_word_limit = 30
+        available_files = [fname for fname in os.listdir(corpus_folder)]
 
+        # Assumes openSubs format
+        corpus = [
+            (file_name.rsplit(".", 4)[0], get_file(file_name))
+            for file_name in available_files
+        ]
 
-def find_examples(ex_pattern, corpus, result_queue):
-    """Find examples in the corpus that match the pattern. The examples should be put into the result_queue, as this is called from a sub-process.
+        print("Corpus prepared with", len(corpus), "files")
+        return np.array(corpus)
 
-    Parameters
-    ----------
-    ex_pattern : re.Pattern
-        The pattern to match
-    corpus : np.array
-        The corpus to search in with tuples of (file_name, file_content)
-    result_queue : AutoProxy[Queue]
-        Multiprocessing Queue to put the results in
-    """
-    found_examples = []
-    for file_name, file_content in corpus:
-        for line in file_content:
-            # Check case insensitive, and sentence should not go over word limit
-            contains_word = ex_pattern.search(line)
-            # Find start and end of match
-            line_split = line.split()
-            under_word_limit = len(line_split) < sentence_word_limit
-            over_min_words = len(line_split) > sentence_min_words
-            if contains_word and under_word_limit and over_min_words:
-                start, end = ex_pattern.search(line).span()
-                found_examples.append((file_name, line, len(line_split), start, end))
-
-    result_queue.put(found_examples)
-
-
-def process_mp(corpus_np, example, num_processes=6):
-    # Create and start the processes
-    processes = []
-    split_size = len(corpus_np) // num_processes
-    with Manager() as manager:
-        result_queue = manager.Queue()
+    def find_examples(self, example):
 
         ex_pattern = re.compile(rf"\b{example}\b", re.IGNORECASE)
-        for i in range(num_processes):
-            split_corpus = corpus_np[i * split_size : (i + 1) * split_size]
-            print(f"Process {i}: Assigning corpus length", len(split_corpus))
-            
-            p = mp.Process(target=find_examples, args=(ex_pattern, split_corpus, result_queue))
+
+        def process_search(corpus_split):
+            """Find examples in the corpus that match the pattern. The examples should be put into the result_queue, as this is called from a sub-process."""
+            found_examples = []
+            for file_name, file_content in corpus_split:
+                for line in file_content:
+                    # Check case insensitive, and sentence should not go over word limit
+                    contains_word = ex_pattern.search(line)
+                    # Find start and end of match
+                    line_split = line.split()
+                    under_word_limit = len(line_split) < self.sentence_word_limit
+                    over_min_words = len(line_split) > self.sentence_min_words
+                    if contains_word and under_word_limit and over_min_words:
+                        start, end = ex_pattern.search(line).span()
+                        found_examples.append(
+                            (file_name, line, len(line_split), start, end)
+                        )
+
+            self.result_queue.put(found_examples)
+
+        # Create and start the processes
+        processes = []
+
+        for i in range(self.num_processes):
+            split_corpus = self.corpus[i * self.split_size : (i + 1) * self.split_size]
+
+            p = mp.Process(target=process_search, args=(split_corpus,))
             p.start()
             processes.append(p)
 
         # Wait for all processes to finish
         for p in processes:
-            print("Waiting for process", p.pid)
             p.join()
-        
-        print("All processes finished")
 
         # Collect the results from the Queue and concatenate them into a single list
         final_result = []
-        while not result_queue.empty():
-            final_result.extend(result_queue.get())
+        while not self.result_queue.empty():
+            final_result.extend(self.result_queue.get())
 
-    final_result = sorted(final_result, key=lambda x: x[2], reverse=True)
-    return final_result
+        final_result = sorted(final_result, key=lambda x: x[2], reverse=True)
+        return final_result
 
 
 if __name__ == "__main__":
-    corpus = prepare_corpus(output_folder)
-    print("Total Examples", sum([len(c[1]) for c in corpus]))
+    corpus_folder = "/media/ducha/SSDSHARED/VN/subs_dump/viet_subs_processed2"
+    corpus_examples = CorpusExamples(corpus_folder)
 
     example = "cô gái"
 
-    num_processes = 6
+    found_examples = corpus_examples.find_examples(example)
 
-    print(process_mp(corpus, example, num_processes))
+    print(found_examples)
     print("Done")
